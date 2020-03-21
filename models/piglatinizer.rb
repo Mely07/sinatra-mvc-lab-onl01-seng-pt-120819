c0567dfdfd52f2c615718f76dd8bae325c1e1b2f
Damely Tineo <damely.tineo07@gmail.com>
2020-03-21 21:44:59 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,10 @@ class PigLatinizer
     words = sentence.split(' ')
     
     words.map! {|word|
-      if word[0].match(/[aeiou]/)
+      if word[0].match /[aeiou]/
         word + "way"
       else
-        arr = word.split/([aeiou].*)/ 
+        #arr = word.split/([aeiou].*)/ 
         
         
       end
