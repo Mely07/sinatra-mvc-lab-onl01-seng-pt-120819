9792b882a8eabd82e9fe09f293ea834e6edd4463
Damely Tineo <damely.tineo07@gmail.com>
2020-03-21 21:21:10 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,12 +4,14 @@ class PigLatinizer
     words = sentence.split(' ')
     
     words.each {|word|
+    
     if word[0].match[/aeiou/] 
       word + 'way'
     else
       arr = word.split/([aeiou].*)/ 
       arr[1] + arr[0]
     end}
+
   end
  
 end
