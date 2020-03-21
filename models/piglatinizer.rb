6a6955a4d11c93e408f7039367fe70aaa97d6c1e
Damely Tineo <damely.tineo07@gmail.com>
2020-03-21 21:47:09 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,19 +12,6 @@ class PigLatinizer
         
       end
     }
-    puts words
-    
-    
-    # if word[0].match([/aeiou/]) 
-    #   word + 'way'
-    # else
-    #   arr = word.split/([aeiou].*)/ 
-    #   arr[1] + arr[0]
-    # end
-
   end
  
 end
-
-sentence = PigLatinizer.new 
-sentence.piglatinize('noodle soup apple')
